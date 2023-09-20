b353be11aab491ad14a74af83750674dc7694185
asmita-coder <64037086+asmita-coder@users.noreply.github.com>
2023-09-21 00:36:49 +0530

    Update streamlit_app.py
    making fruityvice api response presentable

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display table on page
 streamlit.dataframe(fruits_to_show)
 
+# New Section to display fruityvice API response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
